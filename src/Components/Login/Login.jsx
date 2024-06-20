6aca25e085adbea3702c5d9f447b5c7e5f1c3a8e
Wilson <wilsondarocc@gmail.com>
2024-06-20 15:05:52 -0300

    First commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,48 @@
+import {FaUser, FaLock} from 'react-icons/fa';
+import { useState } from 'react';
+import "./Login.css";
+
+const Login = () => {
+    const[username, setUsername] = useState("");
+    const[password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("Enviando os dados" + username + " - " + password);
+    }
+
+  return (
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <img className='logo' src="src\assets\bibliotecalogo2.png" alt="logosite"></img>
+        <div className='input-field'>
+            <input type="email" placeholder='E-mail' 
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <FaUser className='icon'/>
+        </div>
+        <div className='input-field'>
+            <input type="password" placeholder='Senha' 
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <FaLock className='icon'/>
+        </div>
+
+        <div className="recall-forget">
+            <label>
+                <input type="checkbox" />
+                Lembre de mim
+            </label>
+            <a href='#'>Esqueceu a senha?</a>
+        </div>
+
+        <button>Entrar</button>
+
+        <div className="signup-link">
+            <p> Não tem uma conta? <a href='cadastro.html'>Registrar</a></p>
+        </div>
+      </form>
+    </div>
+  )
+}
+export default Login
---
+++ src/cadastro.jsx
@@ -0,0 +1,76 @@
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const confirmar = document.getElementById('confirmar');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checarInputs();
+});
+
+function checarInputs(){
+    const nomeValue = nome.value;
+    const emailValue = email.value;
+    const senhaValue = senha.value;
+    const confirmarValue = confirmar.value;
+
+    if(nomeValue === ""){
+        setErroFor(nome, "O nome é obrigatório.");
+    } else {
+        setSucessoFor(nome);
+    }
+
+    if(emailValue === ""){
+        setErroFor(email, "O email é obrigatório.");
+    } else if (!checkEmail(emailValue)){
+        setErroFor(email, "Insira um e-mail válido.")
+    } else {
+        setSucessoFor(email);
+    }
+
+    if(senhaValue === ''){
+        setErroFor(senha, 'A senha é obrigatória');
+    } else if(senhaValue.length < 7) {
+        setErroFor(senha, 'A senha precisa ter no mínimo 7 caracteres.');
+    } else {
+        setSucessoFor(senha);
+    }
+
+    if(confirmarValue === ''){
+        setErroFor(confirmar, "A confirmação da senha é obrigatória.");
+    } else if (confirmarValue !== senhaValue){
+        setErroFor(confirmar, "As senhas precisam ser iguais.");
+    } else {
+        setSucessoFor(confirmar);
+    }
+
+    const formControls = form.querySelectorAll(".form-control");
+
+    const formIsValid = [...formControls].every((formControl) => {
+        return formControl.className === "form-control sucesso";
+    });
+
+    if (formIsValid) {
+        window.location.href = "index.html";
+    }
+
+}    
+    
+function setErroFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    small.innerText = message;
+
+    formControl.className = 'form-control erro';
+}
+
+function setSucessoFor(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control sucesso";
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
